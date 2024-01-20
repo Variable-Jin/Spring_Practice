8c84fe331f78552ee06cd130b06ed86f624091d6
Variable-Jin <tnwls010223@gmail,com>
2024-01-21 00:37:20 +0900

    crud_db test.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into article(id, title, content) values (1, '은하철도', '999');
+insert into article(id, title, content) values (2, '마바사아', '9012');
+insert into article(id, title, content) values (3, '자차카타', '3456');
